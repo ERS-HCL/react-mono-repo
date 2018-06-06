f7a08631748ec5bcba2b3549ea2f30d3d4ec62e8
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-06-06 16:27:34 +0530

    Main app startup added

--- packages/styleguide/src/index.js
+++ packages/styleguide/src/index.js
@@ -1,15 +1,16 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Catalog, pageLoader } from 'catalog';
-import ComponentOne from 'components/lib/ComponentOne'
-import ComponentTwo from 'components/lib/ComponentTwo'
+import ComponentOne from 'components/lib/ComponentOne';
+import ComponentTwo from 'components/lib/ComponentTwo';
 
 ReactDOM.render(
   <Catalog
     title="Catalog"
     pages={[
       {
-        imports: { ComponentOne, ComponentTwo },  
+        imports: { ComponentOne, ComponentTwo },
         path: '/',
         title: 'Introduction',
         content: pageLoader('intro.md')
